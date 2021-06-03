9b09ad6b89062a01de11976ad125bb0dd0fd92ed
Aobatake1515 <aobatake1515@gmail.com>
2021-06-03 08:42:42 -0700

    Simple camera added
    Added a simple camera that follows the player. Its follow effect gets stronger the further it is from the player.

---
+++ Assets/Scripts/PlayerCameraMB.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraMB : MonoBehaviour
+{
+    public float planeZ;
+    public GameObject target;
+    public float speedPerDist; // (units / sec) / unit apart
+    [Header("Info Only")]
+    public float planePosX;
+    public float planePosY; // pos on z plane
+
+
+    public Vector3 targetPos => target.transform.position;
+    public int width => Camera.main.pixelWidth;
+    public int height => Camera.main.pixelHeight;
+
+    public void Start()
+    {
+    }
+
+    public void FixedUpdate()
+    {
+        UpdatePosition(Time.fixedDeltaTime);
+    }
+
+    public void UpdatePosition(float dt)
+    {
+        UpdatePlanePos();
+
+        Vector3 toTarget = new Vector3(targetPos.x, targetPos.y, planeZ) - new Vector3(planePosX, planePosY, planeZ);
+        Vector3 moveVec = toTarget.normalized * toTarget.magnitude * speedPerDist;
+
+        transform.position = transform.position + moveVec;
+    }
+
+    private void UpdatePlanePos()
+    {
+        planePosX = transform.position.x;
+        planePosY = transform.position.y + (transform.position.z - planeZ) * Mathf.Tan(Mathf.Deg2Rad * transform.rotation.eulerAngles.x);
+    }
+
+}
