bacc557f4bd98bbb340ed5fd729c6602e70e7f30
lizxiuan <lizixuan@usc.edu>
2021-06-10 15:52:21 -0700

    Added colors and velocity threshold to obstacles

--- Assets/Scripts/BreakableObastacle.cs
+++ Assets/Scripts/BreakableObastacle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,12 @@ using UnityEngine;
 public class BreakableObastacle : MonoBehaviour
 {
     public GameObject m;
+    private int hitPoint = 3;//After hitPoint tims collision with the ball, the obstacle will be destroied.
+    public uint collisionDirection;//2 -- horizontal, 1 -- vertical, 3 -- both
+    private float collisionVelocityThreshold = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Instantiate(m, Vector3.up, Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -19,7 +22,33 @@ public class BreakableObastacle : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Ball")
-            Destroy(this.gameObject);
+        if (collision.gameObject.name == "Ball" && CollisionVelocityCheck(collision.rigidbody.velocity))
+        {
+            hitPoint--;
+            if (hitPoint == 0)
+                Destroy(this.gameObject);
+            else if (hitPoint == 1)
+                this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            else if (hitPoint == 2)
+                this.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+
+    private bool CollisionVelocityCheck(Vector2 otherVelocity)
+    {
+        if (collisionDirection == 1)
+        {
+            return Math.Abs(otherVelocity.x) >= collisionVelocityThreshold;
+        }
+        else if (collisionDirection == 2)
+        {
+            return Math.Abs(otherVelocity.y) >= collisionVelocityThreshold;
+        }
+        else if (collisionDirection == 3)
+        {
+            return Math.Max(Math.Abs(otherVelocity.x), Math.Abs(otherVelocity.y)) >= collisionVelocityThreshold;
+        }
+        else
+            return false;
     }
 }
