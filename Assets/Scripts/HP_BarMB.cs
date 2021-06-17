f95f5fd8b9c1825425c193c3c55ece74b8f5df38
Aobatake1515 <aobatake1515@gmail.com>
2021-06-17 08:07:23 -0700

    Improved HP bar with damage delay indicator

--- Assets/Scripts/HP_BarMB.cs
+++ Assets/Scripts/HP_BarMB.cs
@@ -2,27 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using TMPro;
 
 public class HP_BarMB : MonoBehaviour
 {
-    public Slider slider;
+    public Slider hpSlider, delaySlider;
+    public float delayTime; // sec of delay
+    public float delaySliderSpd; // ratio of whole slider / sec
+    public DateTime delayStart;
     public TextMeshProUGUI tmpro;
+
+    private bool isDelay;
 
     public void InitHP(float hp)
     {
-        slider.maxValue = hp;
+        hpSlider.maxValue = hp;
+        delaySlider.maxValue = hp;
         Reset();
     }
 
     public void Reset()
     {
-        SetHP(slider.maxValue);
+        SetHP(hpSlider.maxValue);
+        isDelay = false;
     }
 
-    public void SetHP(float hp)
+    public void SetHP(float hp, bool useDelay = true)
     {
-        slider.value = hp;
-        tmpro.text = slider.value.ToString("N0");
+        hpSlider.value = hp;
+        if (hp >= delaySlider.value)
+        {
+            delaySlider.value = hp;
+        }
+        else if (useDelay)
+        {
+            delayStart = DateTime.Now;
+            isDelay = true;
+        }
+        if (tmpro != null) tmpro.text = hpSlider.value.ToString("F0");
+    }
+
+    public void Update()
+    {
+        if (isDelay)
+        {
+            if ((DateTime.Now - delayStart).TotalSeconds >= delayTime)
+                isDelay = false;
+        }
+        else
+        {
+            if (hpSlider.value < delaySlider.value)
+            {
+                float valDiff = Time.deltaTime * delaySliderSpd * delaySlider.maxValue;
+                delaySlider.value -= valDiff;
+                if (delaySlider.value < hpSlider.value) delaySlider.value = hpSlider.value;
+            }
+        }
     }
 }
