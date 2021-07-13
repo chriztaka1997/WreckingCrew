578f6b2dc564cd3eaef21438f1d1e6d10950bd05
lizxiuan <lizixuan@usc.edu>
2021-07-13 13:55:58 -0700

    simple GUI

---
+++ Assets/HONETi/mobile_cartoon_GUI/Scripts/switch_scenes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class switch_scenes : MonoBehaviour {
+	
+	public string sceneName = "";
+
+	// Use this for initialization
+	void Start () {
+		Button b = GetComponent<Button> ();
+		if (b != null && sceneName != "")
+		{
+			b.onClick.AddListener(() => { Debug.Log(sceneName); SceneManager.LoadScene(sceneName);});
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
