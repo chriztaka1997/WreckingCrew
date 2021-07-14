e3677a612aafa747b325293285ca450016d1673f
chriztaka1997 <chris.imantaka1997@gmail.com>
2021-07-13 21:37:22 -0700

    Enemy burst on killed

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -38,7 +38,7 @@ public class Bullet : GenericItems
     private new void OnTriggerEnter2D(Collider2D collision)
     {
         base.OnTriggerEnter2D(collision);
-        if ((collision.gameObject.tag != "Enemy" && collision.gameObject.tag != "Player") || Time.time>=timeToDie)
+        if ((collision.gameObject.tag =="Wall") || Time.time>=timeToDie)
         {
             gameObject.SetActive(false);
         }
